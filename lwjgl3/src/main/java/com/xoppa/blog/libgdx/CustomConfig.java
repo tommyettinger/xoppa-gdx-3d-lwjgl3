50a488bd614a7f97c9dca994e1e321e5bbc7fec7
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-03-21 20:58:46 -0700

    Out with .class, in with ::new !
    This allows Graal Native Image to work! But the resulting image is 58.5 MB, which isn't great...

--- lwjgl3/src/main/java/com/xoppa/blog/libgdx/CustomConfig.java
+++ lwjgl3/src/main/java/com/xoppa/blog/libgdx/CustomConfig.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+import java.util.function.Supplier;
+
 /**
  * Stores information needed to describe and launch a stage of the tutorial, as a new window.
  */
@@ -11,11 +13,28 @@ public class CustomConfig extends Lwjgl3ApplicationConfiguration {
     public String name;
     public String data;
     public int width, height;
-    public Class<? extends ApplicationListener> clazz;
-    public CustomConfig(String name, int width, int height, Class<? extends ApplicationListener> clazz) {
-        this(name, width, height, clazz, "");
+    public Supplier<? extends ApplicationListener> ctor;
+
+    /**
+     *
+     * @param name used for the title of the tutorial window
+     * @param width the width of the window to open
+     * @param height the height of the window to open
+     * @param ctor a constructor, method reference, lambda, or other function that returns an ApplicationListener
+     */
+    public CustomConfig(String name, int width, int height, Supplier<? extends ApplicationListener> ctor) {
+        this(name, width, height, ctor, "");
     }
-    public CustomConfig(String name, int width, int height, Class<? extends ApplicationListener> clazz, String data) {
+
+    /**
+     *
+     * @param name used for the title of the tutorial window
+     * @param width the width of the window to open
+     * @param height the height of the window to open
+     * @param ctor a constructor, method reference, lambda, or other function that returns an ApplicationListener
+     * @param data allows associating extra information with this window creator, as a String
+     */
+    public CustomConfig(String name, int width, int height, Supplier<? extends ApplicationListener> ctor, String data) {
         super();
         setTitle(this.name = name);
         setWindowIcon(Files.FileType.Internal
@@ -24,7 +43,7 @@ public class CustomConfig extends Lwjgl3ApplicationConfiguration {
                 , "libgdx32.png"
                 , "libgdx16.png");
         setWindowedMode(this.width = width, this.height = height);
-        this.clazz = clazz;
+        this.ctor = ctor;
         this.data = data;
     }
 
